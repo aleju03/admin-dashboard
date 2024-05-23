bc6aeef2ab14b8ab5f309fb18bb09dc837c5dbaf
Aleju03 <alejimenezu@gmail.com>
2024-05-23 03:05:29 -0600

    version 2

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwIoPab2HbVlP4VmcfZDT3ycs__ybgwbY",
-  authDomain: "cuadernodigital2-f8e0e.firebaseapp.com",
-  projectId: "cuadernodigital2-f8e0e",
-  storageBucket: "cuadernodigital2-f8e0e.appspot.com",
-  messagingSenderId: "476802800338",
-  appId: "1:476802800338:web:04a23017d2f86e59b6eb0b"
+  apiKey: "AIzaSyAAle7CIc3ds64c2lSLptl-4juxkyLM4Jg",
+  authDomain: "cuadernodigital-a5a10.firebaseapp.com",
+  projectId: "cuadernodigital-a5a10",
+  storageBucket: "cuadernodigital-a5a10.appspot.com",
+  messagingSenderId: "466543308544",
+  appId: "1:466543308544:web:6e9fc2108473cfc68c8de5"
 };
 
 // Initialize Firebase
