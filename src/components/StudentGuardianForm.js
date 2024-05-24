0aa0177ab83ba40e314f3ec432f134d6c212b2cc
Aleju03 <alejimenezu@gmail.com>
2024-05-24 13:19:07 -0600

    auto password generator

--- src/components/StudentGuardianForm.js
+++ src/components/StudentGuardianForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { db } from '../firebase';
 import { addDoc, collection, doc, updateDoc } from 'firebase/firestore';
 import { DataContext } from '../context/DataContext';
+import { SparklesIcon, EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 
 const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGuardians }) => {
   const { institutions } = useContext(DataContext);
@@ -11,6 +12,8 @@ const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGua
   const [institucion, setInstitucion] = useState('');
   const [estudiantes, setEstudiantes] = useState([]);
   const [nombreEstudiante, setNombreEstudiante] = useState('');
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
   useEffect(() => {
     if (selectedStudentGuardian) {
@@ -21,6 +24,10 @@ const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGua
       setEstudiantes(selectedStudentGuardian.estudiantes || []);
     }
   }, [selectedStudentGuardian]);
+
+  useEffect(() => {
+    setIsFormValid(nombre !== '' && carne !== '' && contraseña !== '' && institucion !== '' && estudiantes.length > 0);
+  }, [nombre, carne, contraseña, institucion, estudiantes]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -38,7 +45,7 @@ const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGua
           estudiantes,
         });
         alert('Encargado actualizado exitosamente');
-        onClose(true); // Indica que se han realizado cambios
+        onClose(true);
         fetchStudentGuardians();
       } catch (error) {
         alert('Error al actualizar el encargado: ' + error.message);
@@ -55,7 +62,7 @@ const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGua
           estudiantes,
         });
         alert('Encargado registrado exitosamente');
-        onClose(true); // Indica que se han realizado cambios
+        onClose(true);
         fetchStudentGuardians();
       } catch (error) {
         alert('Error registrando encargado: ' + error.message);
@@ -76,6 +83,28 @@ const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGua
     setEstudiantes(updatedEstudiantes);
   };
 
+  const generateCarne = () => {
+    const currentYear = new Date().getFullYear();
+    const randomDigits = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
+    const generatedCarne = currentYear.toString() + randomDigits;
+    setCarne(generatedCarne);
+  };
+
+  const generateContraseña = () => {
+    const nameParts = nombre.trim().split(' ');
+    const firstPart = nameParts[0].toLowerCase();
+    const secondInitial = nameParts[1] ? nameParts[1][0].toLowerCase() : '';
+    const randomDigits = Math.floor(Math.random() * 900) + 100; // generates a random 3 digit number
+    const randomLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26)); // generates a random letter
+
+    const generatedContraseña = `${firstPart}${secondInitial}${randomDigits}${randomLetter}`;
+    setContraseña(generatedContraseña);
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-8 shadow-lg max-h-[80vh] overflow-y-auto">
@@ -91,22 +120,49 @@ const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGua
             className="border p-2 mb-2 w-full"
             required
           />
-          <input
-            type="text"
-            value={carne}
-            onChange={(e) => setCarne(e.target.value)}
-            placeholder="Carné del Encargado"
-            className="border p-2 mb-2 w-full"
-            required
-          />
-          <input
-            type="password"
-            value={contraseña}
-            onChange={(e) => setContraseña(e.target.value)}
-            placeholder="Contraseña del Encargado"
-            className="border p-2 mb-2 w-full"
-            required
-          />
+          <div className="relative">
+            <input
+              type="text"
+              value={carne}
+              onChange={(e) => setCarne(e.target.value)}
+              placeholder="Carné del Encargado"
+              className="border p-2 mb-2 w-full pr-10"
+              required
+            />
+            <button
+              type="button"
+              className="absolute right-2 top-[calc(50%-4px)] transform -translate-y-1/2 text-blue-500"
+              onClick={generateCarne}
+            >
+              <SparklesIcon className="h-6 w-6" />
+            </button>
+          </div>
+          <div className="relative">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              value={contraseña}
+              onChange={(e) => setContraseña(e.target.value)}
+              placeholder="Contraseña del Encargado"
+              className="border p-2 mb-2 w-full pr-10"
+              required
+            />
+            {nombre.trim() && (
+              <button
+                type="button"
+                className="absolute right-2 top-[calc(50%-4px)] transform -translate-y-1/2 text-blue-500"
+                onClick={generateContraseña}
+              >
+                <SparklesIcon className="h-6 w-6" />
+              </button>
+            )}
+            <button
+              type="button"
+              className={`absolute ${nombre.trim() ? 'right-12' : 'right-2'} top-[calc(50%-4px)] transform -translate-y-1/2 text-gray-500`}
+              onClick={togglePasswordVisibility}
+            >
+              {passwordVisible ? <EyeIcon className="h-6 w-6" /> : <EyeSlashIcon className="h-6 w-6" />}
+            </button>
+          </div>
           <select
             value={institucion}
             onChange={(e) => setInstitucion(e.target.value)}
@@ -160,8 +216,10 @@ const StudentGuardianForm = ({ onClose, selectedStudentGuardian, fetchStudentGua
           )}
           <button
             type="submit"
-            className={`bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded mr-2 ${!estudiantes.length ? 'opacity-50 cursor-not-allowed' : ''}`}
-            disabled={!estudiantes.length}
+            className={`bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded mr-2 ${
+              !isFormValid ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            disabled={!isFormValid}
           >
             {selectedStudentGuardian ? 'Actualizar' : 'Registrar'}
           </button>
--- src/components/TeacherForm.js
+++ src/components/TeacherForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { db } from '../firebase';
 import { addDoc, collection, doc, updateDoc, getDocs, query, where } from 'firebase/firestore';
 import { DataContext } from '../context/DataContext';
+import { SparklesIcon, EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 
 const TeacherForm = ({ onClose, selectedTeacher, fetchTeachers }) => {
   const { institutions } = useContext(DataContext);
@@ -10,6 +11,7 @@ const TeacherForm = ({ onClose, selectedTeacher, fetchTeachers }) => {
   const [contraseña, setContraseña] = useState('');
   const [institucion, setInstitucion] = useState('');
   const [isFormValid, setIsFormValid] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
   useEffect(() => {
     if (selectedTeacher) {
@@ -26,9 +28,9 @@ const TeacherForm = ({ onClose, selectedTeacher, fetchTeachers }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const institucionRef = doc(db, 'Instituciones', institucion);
-  
+
     if (selectedTeacher) {
       // Editar profesor existente
       try {
@@ -39,7 +41,7 @@ const TeacherForm = ({ onClose, selectedTeacher, fetchTeachers }) => {
           institucion: institucionRef,
         });
         alert('Profesor actualizado exitosamente');
-        onClose(true); // Indica que se han realizado cambios
+        onClose(true);
         fetchTeachers();
       } catch (error) {
         alert('Error al actualizar el profesor: ' + error.message);
@@ -65,12 +67,34 @@ const TeacherForm = ({ onClose, selectedTeacher, fetchTeachers }) => {
           institucion: institucionRef,
         });
         alert('Profesor registrado exitosamente');
-        onClose(true); // Indica que se han realizado cambios
+        onClose(true);
         fetchTeachers();
       } catch (error) {
         alert('Error registrando profesor: ' + error.message);
       }
     }
+  };
+
+  const generateCarne = () => {
+    const currentYear = new Date().getFullYear();
+    const randomDigits = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
+    const generatedCarne = currentYear.toString() + randomDigits;
+    setCarne(generatedCarne);
+  };
+
+  const generateContraseña = () => {
+    const nameParts = nombre.trim().split(' ');
+    const firstPart = nameParts[0].toLowerCase();
+    const secondInitial = nameParts[1] ? nameParts[1][0].toLowerCase() : '';
+    const randomDigits = Math.floor(Math.random() * 900) + 100; // generates a random 3 digit number
+    const randomLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26)); // generates a random letter
+
+    const generatedContraseña = `${firstPart}${secondInitial}${randomDigits}${randomLetter}`;
+    setContraseña(generatedContraseña);
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
   };
 
   return (
@@ -88,22 +112,49 @@ const TeacherForm = ({ onClose, selectedTeacher, fetchTeachers }) => {
             className="border p-2 mb-2 w-full"
             required
           />
-          <input
-            type="text"
-            value={carne}
-            onChange={(e) => setCarne(e.target.value)}
-            placeholder="Carné del Profesor"
-            className="border p-2 mb-2 w-full"
-            required
-          />
-          <input
-            type="password"
-            value={contraseña}
-            onChange={(e) => setContraseña(e.target.value)}
-            placeholder="Contraseña del Profesor"
-            className="border p-2 mb-2 w-full"
-            required
-          />
+          <div className="relative">
+            <input
+              type="text"
+              value={carne}
+              onChange={(e) => setCarne(e.target.value)}
+              placeholder="Carné del Profesor"
+              className="border p-2 mb-2 w-full pr-10"
+              required
+            />
+            <button
+              type="button"
+              className="absolute right-2 top-[calc(50%-4px)] transform -translate-y-1/2 text-blue-500"
+              onClick={generateCarne}
+            >
+              <SparklesIcon className="h-6 w-6" />
+            </button>
+          </div>
+          <div className="relative">
+            <input
+              type={passwordVisible ? "text" : "password"}
+              value={contraseña}
+              onChange={(e) => setContraseña(e.target.value)}
+              placeholder="Contraseña del Profesor"
+              className="border p-2 mb-2 w-full pr-10"
+              required
+            />
+            {nombre.trim() && (
+              <button
+                type="button"
+                className="absolute right-2 top-[calc(50%-4px)] transform -translate-y-1/2 text-blue-500"
+                onClick={generateContraseña}
+              >
+                <SparklesIcon className="h-6 w-6" />
+              </button>
+            )}
+            <button
+              type="button"
+              className={`absolute ${nombre.trim() ? 'right-12' : 'right-2'} top-[calc(50%-4px)] transform -translate-y-1/2 text-gray-500`}
+              onClick={togglePasswordVisibility}
+            >
+              {passwordVisible ? <EyeIcon className="h-6 w-6" /> : <EyeSlashIcon className="h-6 w-6" />}
+            </button>
+          </div>
           <select
             value={institucion}
             onChange={(e) => setInstitucion(e.target.value)}
@@ -119,14 +170,16 @@ const TeacherForm = ({ onClose, selectedTeacher, fetchTeachers }) => {
           </select>
           <button
             type="submit"
-            className={`bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded mr-2 ${!isFormValid ? 'opacity-50 cursor-not-allowed' : ''}`}
+            className={`bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded mr-2 ${
+              !isFormValid ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
             disabled={!isFormValid}
           >
             {selectedTeacher ? 'Actualizar' : 'Registrar'}
           </button>
           <button
             type="button"
-            onClick={() => onClose(false)} // Indica que no se han realizado cambios
+            onClick={() => onClose(false)}
             className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded"
           >
             Cerrar
