a0bfc01fa121e604fd1089ddaecdd14573d63d65
Aleju03 <alejimenezu@gmail.com>
2024-06-16 02:00:00 -0600

    Refactor: Move Firebase config to environment variables
    Moved hardcoded Firebase API keys to environment variables for better security practices. Added .env.example template for documentation.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAle7CIc3ds64c2lSLptl-4juxkyLM4Jg",
-  authDomain: "cuadernodigital-a5a10.firebaseapp.com",
-  projectId: "cuadernodigital-a5a10",
-  storageBucket: "cuadernodigital-a5a10.appspot.com",
-  messagingSenderId: "466543308544",
-  appId: "1:466543308544:web:6e9fc2108473cfc68c8de5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
